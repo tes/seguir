df8ca134c099d75231b7ab2af169b2296761f383
cliftonc <clifton.cunningham@gmail.com>
2015-01-25 20:21:55 +0000

    Add index on application token

--- setup/setupSeguir.js
+++ setup/setupSeguir.js
@@ -6,16 +6,19 @@ var async = require('async');
 function setup(client, keyspace, next) {
 
   var tables =['CREATE TABLE ' + keyspace + '.applications (name text, apptoken uuid, PRIMARY KEY (name))'];
+  var indexes = ['CREATE INDEX ON ' + keyspace + '.applications(apptoken)'];
 
   var helpers = require('./helpers')(client, {
     KEYSPACE: keyspace,
-    tables: tables
+    tables: tables,
+    indexes: indexes
   });
 
   async.series([
     helpers.dropKeyspace,
     helpers.createKeyspace,
-    helpers.createTables
+    helpers.createTables,
+    helpers.createSecondaryIndexes
   ], function(err, data) {
     /* istanbul ignore if */
     if(err) console.dir(err);
