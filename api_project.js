c714ba3e0422f1938575414077fd39a836a17ce5
cliftonc <clifton.cunningham@gmail.com>
2015-01-26 17:24:36 +0000

    Update docs

--- api_project.js
+++ api_project.js
@@ -1,12 +1,12 @@
 define({
   "name": "seguir",
-  "version": "0.1.6",
+  "version": "0.1.7",
   "description": "Social network backend",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-01-26T17:17:46.019Z",
+    "time": "2015-01-26T17:24:20.468Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
