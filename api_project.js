15fd5f5a683a0ffa3a5d87addb651d48d80ac34d
cliftonc <clifton.cunningham@gmail.com>
2015-02-02 07:39:25 +0000

    Update docs with relationship example

--- api_project.js
+++ api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-02-02T07:36:36.044Z",
+    "time": "2015-02-02T07:39:16.722Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
