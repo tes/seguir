0ec5bb42ae1986c6c11a91abcbf0074a7c0858a9
cliftonc <clifton.cunningham@gmail.com>
2015-02-09 16:11:38 +0000

    Update docs properly this time

--- doc/server/api_project.js
+++ doc/server/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-02-09T16:03:05.613Z",
+    "time": "2015-02-09T16:11:12.432Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
