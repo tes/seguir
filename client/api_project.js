eb8df21f46b0bb4063ecbd805b7dc982c05e4be9
cliftonc <clifton.cunningham@gmail.com>
2015-04-10 09:08:25 +0100

    Update docs

--- client/api_project.js
+++ client/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-02-09T16:11:12.866Z",
+    "time": "2015-04-10T08:08:17.093Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
--- server/api_project.js
+++ server/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-02-09T16:11:12.432Z",
+    "time": "2015-04-10T08:08:16.490Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
