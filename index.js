8f1a2a38033ddf379cb0a7c429adc86ec5ce46eb
cliftonc <clifton.cunningham@gmail.com>
2015-05-13 06:46:47 +0100

    Export urls for use in middleware

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var apiManage = require('./api/manage');
 var apiQuery = require('./api/query');
 var apiAuth = require('./api/auth');
+var urls = require('./api/urls');
 var KEYSPACE = 'seguir';
 
 module.exports = function(client, messaging, keyspace) {
   return {
     manage: apiManage(client, messaging),  // Get their keyspace via the request
     query: apiQuery(client, messaging),    // Get their keyspace via the request
-    auth: apiAuth(client, messaging, keyspace || KEYSPACE)
+    auth: apiAuth(client, messaging, keyspace || KEYSPACE),
+    urls: urls
   };
 };
