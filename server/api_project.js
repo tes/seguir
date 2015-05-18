c6e2e3ab0548809f6153fdb8838a97ea6a7a72e9
cliftonc <clifton.cunningham@gmail.com>
2015-05-18 08:57:23 +0100

    Update documents

--- server/api_project.js
+++ server/api_project.js
@@ -1,13 +1,1 @@
-define({
-  "name": "seguir",
-  "version": "0.1.11",
-  "description": "Social network backend",
-  "sampleUrl": false,
-  "apidoc": "0.2.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2015-05-08T12:01:11.459Z",
-    "url": "http://apidocjs.com",
-    "version": "0.12.1"
-  }
-});
+define({  "name": "seguir",  "version": "0.1.21",  "description": "Social network backend",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-05-18T07:55:23.510Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
