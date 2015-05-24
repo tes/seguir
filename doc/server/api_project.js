98dead9e4fed47fe63493c4909b4a298738876ef
cliftonc <clifton.cunningham@gmail.com>
2015-05-24 12:42:24 +0100

    Update docs post semistandard

--- doc/server/api_project.js
+++ doc/server/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "seguir",  "version": "0.1.25",  "description": "Social network backend",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-05-24T06:47:54.172Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
+define({  "name": "seguir",  "version": "0.1.25",  "description": "Social network backend",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-05-24T11:42:15.440Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
