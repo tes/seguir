8e0af1434e485ffb08c1282a24d7a0fb95682033
cliftonc <clifton.cunningham@gmail.com>
2015-05-24 08:41:44 +0100

    Begin slow application of semistandard ... continued

--- api/urls.js
+++ api/urls.js
@@ -1,45 +1,44 @@
 /**
  * Templates for the urls shared between client and server
  */
-var st = require('string-template');
 var _ = require('lodash');
 
 var urls = {
-  addUser:                    '/user',
-  getUser:                    '/user/:user',
-  getUserByName:              '/username/:username',
-  getUserByAltId:             '/useraltid/:altid',
-  getUserRelationship:        '/user/:user/relationship',
-  addPost:                    '/post',
-  getPost:                    '/post/:post',
-  removePost:                 '/post/:post',
-  addFriend:                  '/friend',
-  getFriend:                  '/friend/:friend',
-  removeFriend:               '/user/:user/friend/:user_friend',
-  getFriends:                 '/user/:user/friends',
-  addFriendRequest:           '/friend-request',
-  getFriendRequests:          '/friend-request/active',
-  acceptFriendRequest:        '/friend-request/accept',
-  addFollower:                '/follower',
-  getFollow:                  '/follower/:follow',
-  removeFollower:             '/user/:user/follower/:user_follower',
-  getFollowers:               '/user/:user/followers',
-  addLike:                    '/like',
-  getLike:                    '/like/:like',
-  checkLike:                  '/user/:user/like/:item',
-  removeLike:                 '/user/:user/like/:item',
-  getFeed:                    '/feed/:user',
-  getUserFeed:                '/feed/:user/direct'
-}
+  addUser: '/user',
+  getUser: '/user/:user',
+  getUserByName: '/username/:username',
+  getUserByAltId: '/useraltid/:altid',
+  getUserRelationship: '/user/:user/relationship',
+  addPost: '/post',
+  getPost: '/post/:post',
+  removePost: '/post/:post',
+  addFriend: '/friend',
+  getFriend: '/friend/:friend',
+  removeFriend: '/user/:user/friend/:user_friend',
+  getFriends: '/user/:user/friends',
+  addFriendRequest: '/friend-request',
+  getFriendRequests: '/friend-request/active',
+  acceptFriendRequest: '/friend-request/accept',
+  addFollower: '/follower',
+  getFollow: '/follower/:follow',
+  removeFollower: '/user/:user/follower/:user_follower',
+  getFollowers: '/user/:user/followers',
+  addLike: '/like',
+  getLike: '/like/:like',
+  checkLike: '/user/:user/like/:item',
+  removeLike: '/user/:user/like/:item',
+  getFeed: '/feed/:user',
+  getUserFeed: '/feed/:user/direct'
+};
 
-module.exports = function(url, data) {
-  if(urls[url]) {
-    if(data) {
+module.exports = function (url, data) {
+  if (urls[url]) {
+    if (data) {
       var pattern = urls[url];
-      _.keys(data).forEach(function(key) {
-          pattern = pattern.replace(':' + key, data[key] ? data[key] : null);
+      _.keys(data).forEach(function (key) {
+        pattern = pattern.replace(':' + key, data[key] ? data[key] : null);
       });
-      if(data.query) {
+      if (data.query) {
         pattern += '?' + data.query;
       }
       return pattern;
@@ -50,5 +49,5 @@ module.exports = function(url, data) {
     console.log('Unable to locate URL: ' + url);
     return '';
   }
-}
+};
 
