2f0e5a2060bd08d600f88f1b1c209bb354f91494
cliftonc <clifton.cunningham@gmail.com>
2015-05-25 09:30:48 +0100

    Amend docs so that samples are only written on docs, not on every acceptance test

--- client/api_project.js
+++ client/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "seguir",  "version": "0.1.25",  "description": "Social network backend",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-05-24T11:42:17.042Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
+define({  "name": "seguir",  "version": "0.2.1",  "description": "Social network backend",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-05-25T08:29:54.604Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
