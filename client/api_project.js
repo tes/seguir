4a1d703a284cf862dbc5d2448a3eecfd490af02c
cliftonc <clifton.cunningham@gmail.com>
2015-06-12 07:51:05 +0100

    Add optional backfill param to follow, update docs

--- client/api_project.js
+++ client/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "seguir",  "version": "0.2.6",  "description": "Social network backend",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-06-10T08:56:57.272Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
+define({  "name": "seguir",  "version": "0.3.0",  "description": "Social network backend",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2015-06-12T06:06:38.060Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
