28ece26be834117fd40b137df001ee3285eff868
cliftonc <clifton.cunningham@gmail.com>
2015-07-09 08:30:40 +0100

    Auth refactor wip

--- db/cassandra/setup/setupSeguir.js
+++ db/cassandra/setup/setupSeguir.js
@@ -2,15 +2,15 @@
  * Sets up the core
  */
 var async = require('async');
-var schemaVersion = 0;
+var schemaVersion = 1;
 
 function setup (client, keyspace, next) {
 
   var tables = [
     'CREATE TABLE ' + keyspace + '.accounts (account uuid, name text, isadmin boolean, enabled boolean, PRIMARY KEY (account))',
     'CREATE TABLE ' + keyspace + '.account_users (account uuid, username text, password text, enabled boolean, PRIMARY KEY (account, username))',
-    'CREATE TABLE ' + keyspace + '.applications (appid uuid, name text, appkeyspace text, appsecret text, account uuid, enabled boolean, PRIMARY KEY (appid))',
-    'CREATE TABLE ' + keyspace + '.application_tokens (appid uuid, appkeyspace text, tokenid uuid,  tokensecret text, enabled boolean, PRIMARY KEY (tokenid))',
+    'CREATE TABLE ' + keyspace + '.applications (appid uuid, name text, appkeyspace text, account uuid, enabled boolean, PRIMARY KEY (appid))',
+    'CREATE TABLE ' + keyspace + '.application_tokens (appid uuid, appkeyspace text, tokenid uuid,  tokensecret text, description text, enabled boolean, PRIMARY KEY (tokenid))',
     'CREATE TABLE ' + keyspace + '.schema_version (version varint, applied timestamp, description text, PRIMARY KEY (version, applied)) WITH CLUSTERING ORDER BY (applied DESC)'
   ];
 
