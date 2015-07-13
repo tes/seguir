a9e4ffa44a5fe75699ed4be9e063450610e288ef
cliftonc <clifton.cunningham@gmail.com>
2015-07-13 10:56:41 +0100

    Add new schema migrations

---
+++ db/cassandra/migrations/tenant/3_feed-from-follower.js
@@ -0,0 +1,22 @@
+var async = require('async');
+
+function apply (keyspace, api, next) {
+
+  var addFromFollowerCql = [
+    'ALTER TABLE ' + keyspace + '.feed_timeline ADD from_follow uuid',
+    'ALTER TABLE ' + keyspace + '.user_timeline ADD from_follow uuid',
+    'CREATE INDEX ON ' + keyspace + '.feed_timeline(from_follow)',
+    'CREATE INDEX ON ' + keyspace + '.user_timeline(from_follow)'
+  ];
+  async.mapSeries(addFromFollowerCql, api.client.execute, next);
+
+}
+
+function rollback (keyspace, api, next) {
+  next();
+}
+
+module.exports = {
+  apply: apply,
+  rollback: rollback
+};
