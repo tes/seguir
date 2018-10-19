f1a7b847bb29b9c722db849a0c2b2b8cbad1c5c5
Ranjeetkaur <ranjeet.kaur@tesglobal.com>
2018-10-19 14:57:24 +0100

    migrate to es6-group

--- api/group/index.js
+++ api/group/index.js
@@ -1,9 +1,9 @@
-var async = require('async');
-var _mapValues = require('lodash/mapValues');
-var _zipObject = require('lodash/zipObject');
-var _filter = require('lodash/filter');
+const async = require('async');
+const _mapValues = require('lodash/mapValues');
+const _zipObject = require('lodash/zipObject');
+const _filter = require('lodash/filter');
 
-var DEFAULT_PAGESIZE = 50;
+const DEFAULT_PAGESIZE = 50;
 
 /**
  * This is a collection of methods that allow you to create, update and delete social items.
@@ -13,18 +13,18 @@ var DEFAULT_PAGESIZE = 50;
  * with an SSO flow).
  *
  */
-module.exports = function (api) {
-  var client = api.client;
-  var messaging = api.messaging;
-  var q = client.queries;
+module.exports = (api) => {
+  const client = api.client;
+  const messaging = api.messaging;
+  const q = client.queries;
 
-  function joinGroup (keyspace, group, user, timestamp, cb) {
-    var memberValues = [group, user, timestamp];
-    client.execute(q(keyspace, 'upsertMember'), memberValues, function (err) {
+  const joinGroup = (keyspace, group, user, timestamp, cb) => {
+    const memberValues = [group, user, timestamp];
+    client.execute(q(keyspace, 'upsertMember'), memberValues, (err) => {
       if (err) return cb(err);
-      getGroup(keyspace, group, function (err, result) {
+      getGroup(keyspace, group, (err, result) => {
         if (err) return cb(err);
-        var joinGroupContent = {
+        const joinGroupContent = {
           category: 'social-group',
           type: 'new-member',
           data: {
@@ -40,23 +40,23 @@ module.exports = function (api) {
         });
       });
     });
-  }
+  };
 
-  function addGroup (keyspace, groupName, supergroupId, user, timestamp, options, next) {
+  const addGroup = (keyspace, groupName, supergroupId, user, timestamp, options, next) => {
     if (!next) {
       next = options;
       options = {};
     }
 
-    var groupData = options.groupData || {};
-    var group = client.isValidId(options.group) ? options.group : client.generateId();
+    let groupData = options.groupData || {};
+    const group = client.isValidId(options.group) ? options.group : client.generateId();
 
-    groupData = _mapValues(groupData, function (value) {
+    groupData = _mapValues(groupData, (value) => {
       return value.toString();
     }); // Always ensure our groupdata is <text,text>
 
     // Check group doesn't already exist with this name in this supergroup
-    getGroupByNameWithinSupergroup(keyspace, groupName, supergroupId, function (err, existingGroup) {
+    getGroupByNameWithinSupergroup(keyspace, groupName, supergroupId, (err, existingGroup) => {
       if (err && err.statusCode !== 404) { return next(err); }
       if (existingGroup) {
         return next({
@@ -65,100 +65,100 @@ module.exports = function (api) {
         });
       }
 
-      var groupValues = [group, groupData, groupName, supergroupId];
+      const groupValues = [group, groupData, groupName, supergroupId];
 
-      client.execute(q(keyspace, 'upsertGroup'), groupValues, {}, function (err, result) {
+      client.execute(q(keyspace, 'upsertGroup'), groupValues, {}, (err, result) => {
         if (err) { return next(err); }
-        joinGroup(keyspace, group, user, timestamp, function (err, result) {
+        joinGroup(keyspace, group, user, timestamp, (err, result) => {
           if (err) { return next(err); }
           next(null, _zipObject(['group', 'groupData', 'groupName', 'supergroupId'], groupValues));
         });
       });
     });
-  }
+  };
 
-  function getGroupByNameWithinSupergroup (keyspace, groupName, supergroupId, next) {
-    client.get(q(keyspace, 'selectGroupByNameAndSupergroup'), [groupName, supergroupId], {}, function (err, result) {
+  const getGroupByNameWithinSupergroup = (keyspace, groupName, supergroupId, next) => {
+    client.get(q(keyspace, 'selectGroupByNameAndSupergroup'), [groupName, supergroupId], {}, (err, result) => {
       if (err) { return next(err); }
       if (!result) { return next(api.common.error(404, 'Unable to find group by groupName: ' + groupName + ' and supergroupId ' + supergroupId)); }
       next(null, result);
     });
-  }
+  };
 
-  function getGroup (keyspace, group, next) {
-    client.get(q(keyspace, 'selectGroupById'), [group], {}, function (err, result) {
+  const getGroup = (keyspace, group, next) => {
+    client.get(q(keyspace, 'selectGroupById'), [group], {}, (err, result) => {
       if (err) { return next(err); }
       if (!result) { return next(api.common.error(404, 'Unable to find group by id: ' + group)); }
       next(null, result);
     });
-  }
+  };
 
-  function getGroups (keyspace, groups, next) {
-    async.map(groups, function (group, cb) {
-      getGroup(keyspace, group, function (err, result) {
+  const getGroups = (keyspace, groups, next) => {
+    async.map(groups, (group, cb) => {
+      getGroup(keyspace, group, (err, result) => {
         if (err) return cb();
         cb(null, result);
       });
     }, next);
-  }
+  };
 
-  function updateGroup (keyspace, userAltid, group, groupName, supergroupId, groupData, next) {
-    getGroup(keyspace, group, function (err, result) {
+  const updateGroup = (keyspace, userAltid, group, groupName, supergroupId, groupData, next) => {
+    getGroup(keyspace, group, (err, result) => {
       if (err) { return next(err); }
 
       if (userAltid.toString() !== result.groupdata.admin) {
         return next(new Error('Unable to update the group, only admin can update it.'));
       }
 
-      groupData = _mapValues(groupData, function (value) {
+      groupData = _mapValues(groupData, (value) => {
         return value.toString();
       }); // Always ensure our groupData is <text,text>
 
-      var groupValues = [groupName, supergroupId, groupData, group];
-      client.execute(q(keyspace, 'updateGroup'), groupValues, {}, function (err, value) {
+      const groupValues = [groupName, supergroupId, groupData, group];
+      client.execute(q(keyspace, 'updateGroup'), groupValues, {}, (err, value) => {
         if (err) { return next(err); }
         next(null, _zipObject(['groupName', 'supergroupId', 'groupData', 'group'], groupValues));
       });
     });
-  }
+  };
 
-  function removeMembers (jobData, cb) {
-    client.execute(q(jobData.keyspace, 'removeMembers'), [jobData.group], function (err) {
+  const removeMembers = (jobData, cb) => {
+    client.execute(q(jobData.keyspace, 'removeMembers'), [jobData.group], (err) => {
       if (err) return cb(err);
       cb(null, { status: 'removed' });
     });
-  }
+  };
 
-  function removeMembersByUser (keyspace, user, next) {
-    client.execute(q(keyspace, 'selectGroupsForUser'), [user], function (err, results) {
+  const removeMembersByUser = (keyspace, user, next) => {
+    client.execute(q(keyspace, 'selectGroupsForUser'), [user], (err, results) => {
       if (err) return next(err);
-      async.each(results, function (member, cb) {
+      async.each(results, (member, cb) => {
         client.execute(q(keyspace, 'removeMember'), [member.group, user], cb);
       }, next);
     });
-  }
+  };
 
-  function removeGroup (keyspace, userAltid, user, group, next) {
-    getGroup(keyspace, group, function (err, result) {
+  const removeGroup = (keyspace, userAltid, user, group, next) => {
+    getGroup(keyspace, group, (err, result) => {
       if (err) { return next(err); }
       if (userAltid.toString() !== result.groupdata.admin) {
         return next(new Error('Unable to remove the group, only admin can remove it.'));
       }
-      var jobData = {
+      const jobData = {
         keyspace: keyspace,
         user: user,
         group: group
       };
 
-      var _removeMembers = function (cb) {
+      const _removeMembers = (cb) => {
         if (messaging.enabled) {
           messaging.submit('seguir-remove-members', jobData, cb);
         } else {
           removeMembers(jobData, cb);
         }
       };
-      var _removeGroup = function (cb) {
-        client.execute(q(keyspace, 'removeGroup'), [group], function (err) {
+      const _removeGroup = (cb) => {
+        client.execute(q(keyspace, 'removeGroup'), [group], (err) => {
           if (err) return cb(err);
           cb(null, { status: 'removed' });
         });
@@ -168,23 +168,23 @@ module.exports = function (api) {
         _removeGroup
       ], next);
     });
-  }
+  };
 
-  function getGroupsByUser (keyspace, user, next) {
-    client.execute(q(keyspace, 'selectGroupsForUser'), [user], function (err, results) {
+  const getGroupsByUser = (keyspace, user, next) => {
+    client.execute(q(keyspace, 'selectGroupsForUser'), [user], (err, results) => {
       if (err) return next(err);
 
       if (results && results.length > 0) {
-        async.map(results, function (group, cb) {
-          getGroup(keyspace, group.group, function (err, result) {
+        async.map(results, (group, cb) => {
+          getGroup(keyspace, group.group, (err, result) => {
             if (err) {
               return cb(null, null);
             }
             cb(null, result);
           });
-        }, function (err, groups) {
+        }, (err, groups) => {
           if (err) { return next(err); }
-          var existingGroups = _filter(groups, function (group) {
+          const existingGroups = _filter(groups, (group) => {
             return group !== null;
           });
           next(null, existingGroups);
@@ -193,53 +193,53 @@ module.exports = function (api) {
         next(null, []);
       }
     });
-  }
+  };
 
-  function getGroupsBySupergroupId (keyspace, supergroupId, liu, options, next) {
+  const getGroupsBySupergroupId = (keyspace, supergroupId, liu, options, next) => {
     if (!next) {
       next = options;
       options = {};
     }
-    var pageState = options.pageState;
-    var pageSize = options.pageSize || DEFAULT_PAGESIZE;
+    const pageState = options.pageState;
+    const pageSize = options.pageSize || DEFAULT_PAGESIZE;
 
-    client.execute(q(keyspace, 'selectGroupsBySupergroupId'), [supergroupId], {pageState: pageState, pageSize: pageSize}, function (err, data, nextPageState) {
+    client.execute(q(keyspace, 'selectGroupsBySupergroupId'), [supergroupId], {pageState: pageState, pageSize: pageSize}, (err, data, nextPageState) => {
       if (err) { return next(err); }
 
       if (data && data.length > 0) {
-        async.map(data, function (item, cb) {
-          api.common.isUserGroupMember(keyspace, liu, item.group, function (err) {
+        async.map(data, (item, cb) => {
+          api.common.isUserGroupMember(keyspace, liu, item.group, (err) => {
             if (!err) {
               item.isMember = true;
             }
             cb();
           });
-        }, function () {
+        }, () => {
           next(null, data, nextPageState);
         });
       } else {
         next(null, []);
       }
     });
-  }
+  };
 
-  function getGroupMembers (keyspace, group, next) {
-    client.execute(q(keyspace, 'selectGroupMembers'), [group], {}, function (err, groupMembers) {
+  const getGroupMembers = (keyspace, group, next) => {
+    client.execute(q(keyspace, 'selectGroupMembers'), [group], {}, (err, groupMembers) => {
       if (err) { return next(err); }
 
-      async.mapSeries(groupMembers, function (member, cb) {
+      async.mapSeries(groupMembers, (member, cb) => {
         api.user.mapUserIdToUser(keyspace, member, ['user'], cb);
       }, next);
     });
-  }
+  };
 
-  function leaveGroup (keyspace, group, user, cb) {
-    var memberValues = [group, user];
-    client.execute(q(keyspace, 'removeMember'), memberValues, function (err) {
+  const leaveGroup = (keyspace, group, user, cb) => {
+    const memberValues = [group, user];
+    client.execute(q(keyspace, 'removeMember'), memberValues, (err) => {
       if (err) return cb(err);
       cb(null, { status: 'removed' });
     });
-  }
+  };
 
   return {
     addGroup: addGroup,
