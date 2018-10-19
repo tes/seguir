412c8c35bd53b99a026e9fda9d804afc45912d98
Ranjeetkaur <ranjeet.kaur@tesglobal.com>
2018-10-19 20:56:24 +0100

    migrate to es6-post

--- api/post/index.js
+++ api/post/index.js
@@ -1,5 +1,5 @@
-var _ = require('lodash');
-var async = require('async');
+const _ = require('lodash');
+const async = require('async');
 
 /**
  * This is a collection of methods that allow you to create, update and delete social items.
@@ -11,97 +11,97 @@ var async = require('async');
  * TODO: Exception may be creating a post on someone elses feed.
  *
  */
-module.exports = function (api) {
-  var client = api.client;
-  var q = client.queries;
+module.exports = (api) => {
+  const client = api.client;
+  const q = client.queries;
 
-  function addPost (keyspace, user, content, content_type, timestamp, visibility, altid, next) {
+  const addPost = (keyspace, user, content, content_type, timestamp, visibility, altid, next) => {
     if (!next) { next = altid; altid = null; }
 
-    var group = null;
-    var post = client.generateId();
+    const group = null;
+    const post = client.generateId();
 
-    var convertedContent = api.common.convertContentToString(content, content_type);
-    var originalContent = api.common.convertContentFromString(convertedContent, content_type);
+    const convertedContent = api.common.convertContentToString(content, content_type);
+    const originalContent = api.common.convertContentFromString(convertedContent, content_type);
     if (!originalContent) { return next(new Error('Unable to parse input content, post not saved.')); }
 
-    var data = [post, user, group, convertedContent, content_type, timestamp, visibility, altid];
-    client.execute(q(keyspace, 'upsertPost'), data, {}, function (err) {
+    const data = [post, user, group, convertedContent, content_type, timestamp, visibility, altid];
+    client.execute(q(keyspace, 'upsertPost'), data, {}, (err) => {
       /* istanbul ignore if */
       if (err) { return next(err); }
 
-      var object = _.zipObject(['post', 'user', 'group', 'convertedContent', 'content_type', 'timestamp', 'visibility', 'altid'], data);
-      api.feed.addFeedItem(keyspace, user, object, 'post', function (err) {
+      const object = _.zipObject(['post', 'user', 'group', 'convertedContent', 'content_type', 'timestamp', 'visibility', 'altid'], data);
+      api.feed.addFeedItem(keyspace, user, object, 'post', (err) => {
         if (err) { return next(err); }
         getPost(keyspace, user, post, true, next);
         api.metrics.increment('post.add');
       });
     });
-  }
+  };
 
-  function _addPostToGroup (keyspace, group, user, content, content_type, timestamp, visibility, altid, next) {
-    var post = client.generateId();
+  const _addPostToGroup = (keyspace, group, user, content, content_type, timestamp, visibility, altid, next) => {
+    const post = client.generateId();
 
-    var convertedContent = api.common.convertContentToString(content, content_type);
-    var originalContent = api.common.convertContentFromString(convertedContent, content_type);
+    const convertedContent = api.common.convertContentToString(content, content_type);
+    const originalContent = api.common.convertContentFromString(convertedContent, content_type);
     if (!originalContent) { return next(new Error('Unable to parse input content, post not saved.')); }
 
-    var data = [post, user, group, convertedContent, content_type, timestamp, visibility, altid];
-    client.execute(q(keyspace, 'upsertPost'), data, {}, function (err) {
+    const data = [post, user, group, convertedContent, content_type, timestamp, visibility, altid];
+    client.execute(q(keyspace, 'upsertPost'), data, {}, (err) => {
       /* istanbul ignore if */
       if (err) { return next(err); }
 
-      var object = _.zipObject(['post', 'user', 'group', 'convertedContent', 'content_type', 'timestamp', 'visibility', 'altid'], data);
-      api.feed.addFeedItemToGroup(keyspace, group, user, object, 'post', function (err) {
+      const object = _.zipObject(['post', 'user', 'group', 'convertedContent', 'content_type', 'timestamp', 'visibility', 'altid'], data);
+      api.feed.addFeedItemToGroup(keyspace, group, user, object, 'post', (err) => {
         if (err) { return next(err); }
         getPost(keyspace, user, post, true, next);
         api.metrics.increment('post.toGroup.add');
       });
     });
-  }
+  };
 
-  function isUserGroupMember (keyspace, user, group, next) {
-    client.get(q(keyspace, 'selectMemberByUserAndGroup'), [user, group], {}, function (err, result) {
+  const isUserGroupMember = (keyspace, user, group, next) => {
+    client.get(q(keyspace, 'selectMemberByUserAndGroup'), [user, group], {}, (err, result) => {
       if (err) { return next(err); }
       if (!result) { return next(api.common.error(404, 'User ' + user + ' is not a member of group ' + group)); }
       next(null, result);
     });
-  }
+  };
 
-  function addPostToGroup (keyspace, group, user, content, content_type, timestamp, visibility, altid, next) {
+  const addPostToGroup = (keyspace, group, user, content, content_type, timestamp, visibility, altid, next) => {
     if (!next) { next = altid; altid = null; }
 
-    isUserGroupMember(keyspace, user, group, function (err) {
+    isUserGroupMember(keyspace, user, group, (err) => {
       if (err) { return next(err); }
       _addPostToGroup(keyspace, group, user, content, content_type, timestamp, visibility, altid, next);
     });
-  }
+  };
 
-  function updatePost (keyspace, user, post, content, content_type, visibility, next) {
-    getPost(keyspace, user, post, function (err, postItem) {
+  const updatePost = (keyspace, user, post, content, content_type, visibility, next) => {
+    getPost(keyspace, user, post, (err, postItem) => {
       if (err) { return next(err); }
       if (postItem.user.user.toString() !== user.toString()) {
         return next(new Error('Unable to update the post, only author can update it.'));
       }
       _updatePost(keyspace, postItem, content, content_type, visibility, next);
     });
-  }
+  };
 
-  function updatePostByAltid (keyspace, altid, content, content_type, visibility, next) {
-    api.common.get(keyspace, 'selectPostByAltid', [altid], 'one', function (err, postItem) {
+  const updatePostByAltid = (keyspace, altid, content, content_type, visibility, next) => {
+    api.common.get(keyspace, 'selectPostByAltid', [altid], 'one', (err, postItem) => {
       if (err) { return next(err); }
       _updatePost(keyspace, postItem, content, content_type, visibility, next);
     });
-  }
+  };
 
-  function _updatePost (keyspace, post, content, content_type, visibility, next) {
-    var convertedContent = api.common.convertContentToString(content, content_type);
-    var originalContent = api.common.convertContentFromString(convertedContent, content_type);
+  const _updatePost = (keyspace, post, content, content_type, visibility, next) => {
+    const convertedContent = api.common.convertContentToString(content, content_type);
+    const originalContent = api.common.convertContentFromString(convertedContent, content_type);
     if (!originalContent) { return next(new Error('Unable to parse input content, post not updated.')); }
 
-    var data = [convertedContent, content_type, visibility, post.post];
+    const data = [convertedContent, content_type, visibility, post.post];
 
-    client.execute(q(keyspace, 'updatePost'), data, {cacheKey: 'post:' + post.post}, function (err, result) {
+    client.execute(q(keyspace, 'updatePost'), data, {cacheKey: 'post:' + post.post}, (err, result) => {
       /* istanbul ignore if */
       if (err) { return next(err); }
       api.metrics.increment('post.update');
@@ -111,68 +111,68 @@ module.exports = function (api) {
         visibility: visibility
       }));
     });
-  }
+  };
 
-  function removePost (keyspace, user, post, next) {
-    getPost(keyspace, user, post, function (err, postItem) {
+  const removePost = (keyspace, user, post, next) => {
+    getPost(keyspace, user, post, (err, postItem) => {
       if (err) { return next(err); }
       if (postItem.user.user.toString() !== user.toString()) {
         return next(new Error('Unable to remove the post, only author can remove it.'));
       }
       _removePost(keyspace, postItem.post, next);
     });
-  }
+  };
 
-  function removePostByAltid (keyspace, user, altid, next) {
-    getPostByAltid(keyspace, user, altid, function (err, postItem) {
+  const removePostByAltid = (keyspace, user, altid, next) => {
+    getPostByAltid(keyspace, user, altid, (err, postItem) => {
       if (err) { return next(err); }
       _removePost(keyspace, postItem.post, next);
     });
-  }
+  };
 
-  function removePostsByAltid (keyspace, user, altid, next) {
-    getPostsByAltid(keyspace, user, altid, function (errGet, posts) {
+  const removePostsByAltid = (keyspace, user, altid, next) => {
+    getPostsByAltid(keyspace, user, altid, (errGet, posts) => {
       if (errGet) { return next(errGet); }
-      async.map(posts, function (postItem, cb) {
+      async.map(posts, (postItem, cb) => {
         _removePost(keyspace, postItem.post, cb);
-      }, function (errRemove, status) {
+      }, (errRemove, status) => {
         next(errRemove, status && status.length ? status[0] : status);
       });
     });
-  }
+  };
 
-  function removePostsByUser (keyspace, user, next) {
-    client.execute(q(keyspace, 'selectPostsByUser'), [user], function (err, results) {
+  const removePostsByUser = (keyspace, user, next) => {
+    client.execute(q(keyspace, 'selectPostsByUser'), [user], (err, results) => {
       if (err) { return next(err); }
-      async.each(results, function (post, cb) {
+      async.each(results, (post, cb) => {
         _removePost(keyspace, post.post, cb);
       }, next);
     });
-  }
+  };
 
-  function _removePost (keyspace, post, next) {
-    var deleteData = [post];
-    client.execute(q(keyspace, 'removePost'), deleteData, {cacheKey: 'post:' + post}, function (err, result) {
+  const _removePost = (keyspace, post, next) => {
+    const deleteData = [post];
+    client.execute(q(keyspace, 'removePost'), deleteData, {cacheKey: 'post:' + post}, (err, result) => {
       if (err) return next(err);
-      api.feed.removeFeedsForItem(keyspace, post, function (err) {
+      api.feed.removeFeedsForItem(keyspace, post, (err) => {
         if (err) return next(err);
         api.metrics.increment('post.remove');
         next(null, {status: 'removed'});
       });
     });
-  }
+  };
 
-  function _validatePost (keyspace, liu, post, expandUser, next) {
+  const _validatePost = (keyspace, liu, post, expandUser, next) => {
     post.content = api.common.convertContentFromString(post.content, post.content_type);
-    api.friend.userCanSeeItem(keyspace, liu, post, ['user'], function (err) {
+    api.friend.userCanSeeItem(keyspace, liu, post, ['user'], (err) => {
       if (err) { return next(err); }
 
-      api.like.checkLike(keyspace, liu, post.post, function (err, likeStatus) {
+      api.like.checkLike(keyspace, liu, post.post, (err, likeStatus) => {
         if (err) { return next(err); }
 
         post.userLiked = likeStatus.userLiked;
         post.likedTotal = likeStatus.likedTotal;
-        api.comment.getComments(keyspace, liu, post.post, function (err, commentsTimeline) {
+        api.comment.getComments(keyspace, liu, post.post, (err, commentsTimeline) => {
           if (err) { return next(err); }
 
           post.commentsTimeline = commentsTimeline;
@@ -180,37 +180,37 @@ module.exports = function (api) {
         });
       });
     });
-  }
+  };
 
-  function getPostFromObject (keyspace, liu, item, next) {
-    var post = api.common.expandEmbeddedObject(item, 'post', 'post');
+  const getPostFromObject = (keyspace, liu, item, next) => {
+    const post = api.common.expandEmbeddedObject(item, 'post', 'post');
     post.user = item.user;
     _validatePost(keyspace, liu, post, true, next);
-  }
+  };
 
-  function getPost (keyspace, liu, post, expandUser, next) {
+  const getPost = (keyspace, liu, post, expandUser, next) => {
     if (!next) { next = expandUser; expandUser = true; }
-    client.get(q(keyspace, 'selectPost'), [post], {cacheKey: 'post:' + post}, function (err, post) {
+    client.get(q(keyspace, 'selectPost'), [post], {cacheKey: 'post:' + post}, (err, post) => {
       if (err) { return next(err); }
       if (!post) { return next({statusCode: 404, message: 'Post not found'}); }
       _validatePost(keyspace, liu, post, expandUser, next);
     });
-  }
+  };
 
-  function getPostByAltid (keyspace, liu, altid, next) {
-    api.common.get(keyspace, 'selectPostByAltid', [altid], 'one', function (err, post) {
+  const getPostByAltid = (keyspace, liu, altid, next) => {
+    api.common.get(keyspace, 'selectPostByAltid', [altid], 'one', (err, post) => {
       if (err) { return next(err); }
       _validatePost(keyspace, liu, post, true, next);
     });
-  }
+  };
 
-  function getPostsByAltid (keyspace, liu, altid, next) {
-    api.common.get(keyspace, 'selectPostByAltid', [altid], 'many', function (err, posts) {
+  const getPostsByAltid = (keyspace, liu, altid, next) => {
+    api.common.get(keyspace, 'selectPostByAltid', [altid], 'many', (err, posts) => {
       if (err) { return next(err); }
-      var userCanSeeItems = [];
-      var userCanSeeItemError = null;
-      async.map(posts, function (post, cb) {
-        _validatePost(keyspace, liu, post, true, function (err2, item) {
+      const userCanSeeItems = [];
+      let userCanSeeItemError = null;
+      async.map(posts, (post, cb) => {
+        _validatePost(keyspace, liu, post, true, (err2, item) => {
           if (err2) {
             userCanSeeItemError = err2;
           } else {
@@ -218,7 +218,7 @@ module.exports = function (api) {
           }
           cb(null);
         });
-      }, function () {
+      }, () => {
         // in the above loop, if _validatePost return error for 1 of item,
         // all the unerrored posts will be ignored in this callback
         // so we need to error only when _validatePost error for all items
@@ -226,7 +226,7 @@ module.exports = function (api) {
         next(null, userCanSeeItems);
       });
     });
-  }
+  };
 
   return {
     addPost: addPost,
