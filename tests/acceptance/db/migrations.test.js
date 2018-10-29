bced67c7dd6b09a635f4f3106ab785b57630b79e
Ranjeetkaur <ranjeet.kaur@tesglobal.com>
2018-10-29 13:32:05 +0000

    migrate to es6-migrations.test

--- tests/acceptance/db/migrations.test.js
+++ tests/acceptance/db/migrations.test.js
@@ -4,35 +4,35 @@
 
 /* eslint-env node, mocha */
 
-var keyspace = 'test_seguir_migrations';
-var Api = require('../../../api');
-var _ = require('lodash');
-var expect = require('expect.js');
-var path = require('path');
-var databases = process.env.DATABASE ? [process.env.DATABASE] : ['cassandra'];
+const keyspace = 'test_seguir_migrations';
+const Api = require('../../../api');
+const _ = require('lodash');
+const expect = require('expect.js');
+const path = require('path');
+const databases = process.env.DATABASE ? [process.env.DATABASE] : ['cassandra'];
 
-databases.forEach(function (db) {
-  var config = _.clone(require('../../fixtures/' + db + '.json'));
+databases.forEach((db) => {
+  const config = _.clone(require('../../fixtures/' + db + '.json'));
   config.keyspace = keyspace;
 
   describe('Seguir Database Migrations - ' + db, function () {
     this.timeout(10000);
     this.slow(5000);
 
-    var api, auth;
+    let api, auth;
 
-    before(function (done) {
+    before((done) => {
       this.timeout(20000);
-      Api(config, function (err, seguirApi) {
+      Api(config, (err, seguirApi) => {
         expect(err).to.be(null);
         api = seguirApi;
         // Fix migration path to fixtures
         api.client.migrations = path.resolve(__dirname, '..', '..', 'fixtures', 'migrations');
         auth = api.auth;
-        api.client.setup.setupSeguir(api.client, keyspace, function () {
-          auth.addAccount('migration account', false, false, function (err, account) {
+        api.client.setup.setupSeguir(api.client, keyspace, () => {
+          auth.addAccount('migration account', false, false, (err, account) => {
             expect(err).to.be(null);
-            auth.addApplication(account.account, 'migration a', function (err, application) {
+            auth.addApplication(account.account, 'migration a', (err, application) => {
               expect(err).to.be(null);
               done();
             });
@@ -41,8 +41,8 @@ databases.forEach(function (db) {
       });
     });
 
-    it('can retrieve current version, and previous versions for core database schema', function (done) {
-      api.migrations.getSchemaVersions(function (err, versions) {
+    it('can retrieve current version, and previous versions for core database schema', (done) => {
+      api.migrations.getSchemaVersions((err, versions) => {
         expect(err).to.be(null);
         // Cassandra stores integers as a type Integer that needs to be converted
         expect(versions[0].version.toString()).to.be('1');
@@ -50,8 +50,8 @@ databases.forEach(function (db) {
       });
     });
 
-    it('can figure out which migrations need to be applied based on schema version', function (done) {
-      api.migrations.getMigrationsToApply(function (err, migrations) {
+    it('can figure out which migrations need to be applied based on schema version', (done) => {
+      api.migrations.getMigrationsToApply((err, migrations) => {
         expect(err).to.be(null);
         expect(migrations[0].type).to.be('seguir');
         expect(migrations[0].version.toString()).to.be('1000');
@@ -65,18 +65,18 @@ databases.forEach(function (db) {
       });
     });
 
-    it('can apply migrations, stopping at any failure and not continuing', function (done) {
-      api.migrations.getMigrationsToApply(function (err, migrations) {
+    it('can apply migrations, stopping at any failure and not continuing', (done) => {
+      api.migrations.getMigrationsToApply((err, migrations) => {
         expect(err).to.be(null);
-        api.migrations.applyMigrations(migrations, function (err) {
+        api.migrations.applyMigrations(migrations, (err) => {
           expect(err.message).to.be('example error');
           done();
         });
       });
     });
 
-    it('can see those migrations left that are after the failed migration', function (done) {
-      api.migrations.getMigrationsToApply(function (err, migrations) {
+    it('can see those migrations left that are after the failed migration', (done) => {
+      api.migrations.getMigrationsToApply((err, migrations) => {
         expect(err).to.be(null);
         expect(migrations[0].type).to.be('tenant');
         expect(migrations[0].version.toString()).to.be('1003');
